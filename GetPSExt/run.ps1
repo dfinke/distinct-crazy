87634a81f8a70e34cd90775bdff805c1e7059b17
dfinke <finked@hotmail.com>
2017-06-19 18:20:09 -0400

    Added

---
+++ GetPSExt/run.ps1
@@ -0,0 +1,13 @@
+$(foreach ($target in "vscode-powershell", "powershelleditorservices") {
+        foreach ($branch in "master") {
+            $url = "https://ci.appveyor.com/api/projects/PowerShell/{0}/branch/{1}" -f $target, $branch
+            (Invoke-RestMethod $url).build | ForEach-Object {
+                [PSCustomObject][Ordered]@{
+                    Repo       = "$target/$branch"
+                    Date       = (get-date $_.committed).ToShortDateString()
+                    authorName = $_.authorName
+                    message    = $_.message
+                }
+            }
+        }
+    }) | Sort-Object date | ConvertTo-JSON | Set-Content $res
